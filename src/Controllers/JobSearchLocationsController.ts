7d869df7942e7d753166948d144c72c7e99311b4
AhmedBHameed <ahmedbazy@gmail.com>
2021-07-13 13:34:01 +0200

    Fix locations api

--- src/Controllers/JobSearchLocationsController.ts
+++ src/Controllers/JobSearchLocationsController.ts
@@ -1,31 +1,31 @@
-import {Permission} from 'accesscontrol';
+// import {Permission} from 'accesscontrol';
 import express, {Request, Response} from 'express';
-import {ac, ParsedTokenData} from '../services/AccessControl';
-import {PERMISSION_DENIED} from '../use-cases/commonErrors';
+// import {ac, ParsedTokenData} from '../services/AccessControl';
+// import {PERMISSION_DENIED} from '../use-cases/commonErrors';
 import getJobSearchLocations from '../use-cases/getJobLocations.ts/getJobLocations.usecase';
 import environment from '../config/environment';
 import {callTryCatch} from '../util/callTryCatch';
-import {ErrorModel} from './shared/models/errorModel';
+// import {ErrorModel} from './shared/models/errorModel';
 
 const {rootPath, apiVersion} = environment;
 const JobSearchLocationsRouter = express.Router();
 
 const GetJobSearchLocationsController = async (req: Request, res: Response) => {
-  const {sessionData, sessionError} = req as Request & {sessionData: ParsedTokenData; sessionError: ErrorModel};
+  // const {sessionData, sessionError} = req as Request & {sessionData: ParsedTokenData; sessionError: ErrorModel};
 
-  if (sessionError) {
-    const {statusCode} = sessionError;
-    res.status(statusCode).send(sessionError);
-    return;
-  }
+  // if (sessionError) {
+  //   const {statusCode} = sessionError;
+  //   res.status(statusCode).send(sessionError);
+  //   return;
+  // }
 
-  const [_, permission] = await callTryCatch(async () => ac.can(sessionData.role).createAny('location'));
+  // const [_, permission] = await callTryCatch(async () => ac.can(sessionData.role).createAny('location'));
 
-  if (!(permission as Permission)?.granted) {
-    const {statusCode} = PERMISSION_DENIED;
-    res.status(statusCode).send(PERMISSION_DENIED);
-    return;
-  }
+  // if (!(permission as Permission)?.granted) {
+  //   const {statusCode} = PERMISSION_DENIED;
+  //   res.status(statusCode).send(PERMISSION_DENIED);
+  //   return;
+  // }
 
   const [error, response] = await callTryCatch(async () => await getJobSearchLocations());
   if (error) {
